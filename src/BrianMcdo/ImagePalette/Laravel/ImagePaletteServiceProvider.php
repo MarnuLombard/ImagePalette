4c4fce14e246606935dfcb1d16f845c860598d27
Marnu Lombard <marnu@blackafricagroup.com>
2015-02-17 14:33:08 +0200

    Make ServiceProvider compatible with Laravel 5

--- src/BrianMcdo/ImagePalette/Laravel/ImagePaletteServiceProvider.php
+++ src/BrianMcdo/ImagePalette/Laravel/ImagePaletteServiceProvider.php
@@ -16,44 +16,54 @@ use BrianMcdo\ImagePalette\Client;
 
 class ImagePaletteServiceProvider extends ServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->package('brianmcdo/image-palette');
-    }
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		/*$this->package('brianmcdo/image-palette');*/
+		// As per BarryVDH :
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app['image-palette'] = $this->app->share(function()
-        {
-            return new Client;
-        });
-    }
+		// Is it possible to register the config?
+		if (method_exists($this->app['config'], 'package')) {
+			$this->app['config']->package('nwidart/db-exporter', __DIR__ . '/../../config');
+		} else {
+			// Load the config for now..
+			$config = $this->app['files']->getRequire(__DIR__ .'/../../config/config.php');
+			$this->app['config']->set('nwidart/db-exporter::config', $config);
+		}
+	}
 
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return string[]
-     */
-    public function provides()
-    {
-        return array('image-palette');
-    }
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['image-palette'] = $this->app->share(function()
+		{
+			return new Client;
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return string[]
+	 */
+	public function provides()
+	{
+		return array('image-palette');
+	}
 
 } 
